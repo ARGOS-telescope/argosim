e8c5a81bdf60c21fce986ed2050bd940b7a6e258
CentofantiEze <ezecentofanti@gmail.com>
2025-07-23 16:58:15 +0200

    Update version in the docs.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,7 +17,7 @@ sys.path.insert(0, os.path.abspath("../.."))
 project = "Argosim"
 copyright = "2024, Ezequiel Centofanti, Emma Aycoberry"
 author = "Ezequiel Centofanti, Emma Aycoberry"
-release = "0.0.1"
+release = "0.0.3"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -35,9 +35,24 @@ extensions = [
 templates_path = ["_templates"]
 exclude_patterns = []
 
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3/", None),
+    "numpy": ("https://numpy.org/doc/stable/", None),
+    "scipy": ("https://docs.scipy.org/doc/scipy/", None),
+    "matplotlib": ("https://matplotlib.org/stable/", None),
+    "JAX": ("https://jax.readthedocs.io/en/latest/", None),
+}
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "sphinx_book_theme"
+html_theme_options = {
+    "repository_url": "https://github.com/ARGOS-telescope/argosim",
+    "use_repository_button": True,
+    "use_issues_button": True,
+    "use_edit_page_button": True,
+    "use_download_button": True,
+    "use_fullscreen_button": True,
+}
 html_static_path = ["_static"]
