776efc0d271f1ff6d152843bb932ef29bfbac5a6
CentofantiEze <ezecentofanti@gmail.com>
2026-08-28 16:06:40 +0200

    Unit test conv gridding.

--- src/argosim/tests/test_imaging_utils.py
+++ src/argosim/tests/test_imaging_utils.py
@@ -1,18 +1,12 @@
 import numpy as np
 import numpy.testing as npt
 
+import argosim
 import argosim.imaging_utils as aiu
 
 
 class TestImagingUtils:
 
-    sky_model_params = (
-        (256, 256),  # shape_px
-        1.0,  # fov
-        [0.01, 0.02, 0.03],  # deg_size_list
-        [0.4, 0.3, 0.3],  # source_intensity_list
-        332,  # seed
-    )
     sky_model_expected_path = "src/argosim/tests/data/sky_model_exp.npy"
     sky_model_uv_expected_path = "src/argosim/tests/data/sky_model_uv_exp.npy"
 
@@ -30,12 +24,8 @@ class TestImagingUtils:
     pathfinder_uv_mask_weighted_path = (
         "src/argosim/tests/data/argos_pathfinder_uv_mask_weighted.npy"
     )
-    uv_weights_path = "src/argosim/tests/data/uv_sampling_weights.npy"
 
     sky_uv_w_masked_path = "src/argosim/tests/data/sky_uv_w_masked.npy"
-
-    uv_noise_params = (0.1, 612)  # noise_level  # seed
-    sky_uv_w_masked_noisy_path = "src/argosim/tests/data/sky_uv_w_masked_noisy.npy"
 
     obs_sim_single_band_path = "src/argosim/tests/data/obs_sim_single_band.npy"
     dirty_beam_sim_single_band_path = (
@@ -48,6 +38,26 @@ class TestImagingUtils:
         "freqs": [1.5e9],
     }
     decimal_uv = 4
+
+    KB_W = 7
+    KB_beta = 2.0
+    KB_x = 1.5
+    KB_result = 0.87684005
+
+    KB_correction_shape = (128, 128)
+    KB_correction_result_path = "src/argosim/tests/data/kb_correction_result.npy"
+
+    continuous_scaling_result_path = "src/argosim/tests/data/continuous_scaling_result.npy"
+
+    gridded_visibilities_conv_result_path = "src/argosim/tests/data/conv_gridded_result.npy"
+
+    degridded_visibilities_conv_result_path = "src/argosim/tests/data/conv_degridded_result.npy"
+
+    uniform_w_expected_path = "src/argosim/tests/data/conv_vis_weights_uniform.npy"
+
+    vis_sigma = 0.1
+    vis_seed = 42
+    noisy_visibilities_path = "src/argosim/tests/data/noisy_visibilities.npy"
 
     def test_sky2uv(self):
         sky = np.load(self.sky_model_expected_path)
@@ -173,4 +183,107 @@ class TestImagingUtils:
             dirty_beam_out_multi[0],
             dirty_beam_exp,
             err_msg="Simulated multi-band dirty beam does not match the expected output.",
+        )
+
+    def test_as_fov_tuple(self):
+        fov_deg = self.obs_sim_single_band_params["fov_size"]
+        fov_tuple = aiu._as_fov_tuple(fov_deg)
+        npt.assert_array_equal(
+            fov_tuple,
+            (fov_deg, fov_deg),
+            err_msg="FOV tuple conversion failed. The resulting tuple does not match the expected output.",
+        )
+
+        npt.assert_almost_equal(
+            fov_tuple,
+            aiu._as_fov_tuple(fov_tuple),
+            err_msg="FOV tuple conversion failed. The resulting tuple does not match the expected output.",
+        )
+
+    def test_kaiser_bessel(self):
+        result = aiu.kaiser_bessel(self.KB_x, self.KB_W, self.KB_beta)
+        npt.assert_almost_equal(
+            result,
+            self.KB_result,
+            decimal=6,
+            err_msg="Kaiser-Bessel function computation failed. The resulting value does not match the expected output.",
+        )
+
+    def test_kb_correction(self):
+        result = aiu.kb_correction(self.KB_correction_shape, self.KB_W, self.KB_beta)
+        expected_result = np.load(self.KB_correction_result_path)
+        npt.assert_array_almost_equal(
+            result,
+            expected_result,
+            decimal=6,
+            err_msg="Kaiser-Bessel correction computation failed. The resulting array does not match the expected output.",
+        )
+
+    def test_scale_uv_samples_continuous(self):
+        uv_samples = np.load(self.pathfinder_uv_track_path)
+        sky_uv_shape = self.KB_correction_shape
+        fov_size = self.obs_sim_single_band_params["fov_size"]
+        result = aiu.scale_uv_samples_continuous(uv_samples, sky_uv_shape, fov_size)
+        result_expected = np.load(self.continuous_scaling_result_path)
+        npt.assert_array_almost_equal(
+            result,
+            result_expected,
+            decimal=6,
+            err_msg="Continuous scaling of UV samples failed. The resulting array does not match the expected output.",
+        )
+
+    def test_grid_visibilities_conv(self):
+        uv_px = np.load(self.continuous_scaling_result_path)
+        vis = np.ones(uv_px.shape[0], dtype=np.complex64)
+        result_grid = aiu.grid_visibilities_conv(vis, uv_px, (128, 128), self.KB_W, self.KB_beta)
+        result_expected = np.load(self.gridded_visibilities_conv_result_path)
+        npt.assert_array_almost_equal(
+            result_grid,
+            result_expected,
+            decimal=6,
+            err_msg="Convolutional gridding of visibilities failed. The resulting grid does not match the expected output.",
+        )
+
+    def test_degrid_visibilities_conv(self):
+        sky_uv = np.load(self.gridded_visibilities_conv_result_path)
+        scaling_result = np.load(self.continuous_scaling_result_path)
+        result_degridded = aiu.degrid_visibilities_conv(sky_uv, scaling_result, (128, 128), self.KB_W, self.KB_beta)
+        result_expected = np.load(self.degridded_visibilities_conv_result_path)
+        npt.assert_array_almost_equal(
+            result_degridded,
+            result_expected,
+            decimal=6,
+            err_msg="Convolutional degridding of visibilities failed. The resulting array does not match the expected output.",
+        )
+
+    def test_visibility_weights(self):
+
+        scaling_result = np.load(self.continuous_scaling_result_path)
+
+        uniform_w_result = aiu._visibility_weights(scaling_result, (128, 128), "uniform")
+        uniform_w_expected = np.load(self.uniform_w_expected_path)
+        npt.assert_array_almost_equal(
+            uniform_w_result,
+            uniform_w_expected,
+            decimal=6,
+            err_msg="Uniform weighting of visibilities failed. The resulting weights do not match the expected output.",
+        )
+
+        # Under natural weighting, all weights should be 1.0
+        natural_w_result_true =(aiu._visibility_weights(scaling_result, (128, 128), "natural") == 1.).all()
+        npt.assert_equal(
+            natural_w_result_true,
+            np.array(True),
+            err_msg="Natural weighting of visibilities failed. The resulting weights do not match the expected output.",
+        )
+
+    def test_add_noise_vis(self):
+        vis = np.load(self.degridded_visibilities_conv_result_path)
+        noisy_vis_out = aiu.add_noise_vis(vis, sigma=self.vis_sigma, seed=self.vis_seed)
+        noisy_vis_expected = np.load(self.noisy_visibilities_path)
+        npt.assert_array_almost_equal(
+            noisy_vis_out,
+            noisy_vis_expected,
+            decimal=6,
+            err_msg="Adding noise to visibilities failed. The resulting noisy visibilities do not match the expected output.",
         )
